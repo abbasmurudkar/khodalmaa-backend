ed017b31b4316e3f939e2821af188631f1df0033
abbas <abbas.murudkar@elixirequties.com>
2025-08-12 13:49:51 +0530

    Try and Except

--- telegram/telegram.py
+++ telegram/telegram.py
@@ -12,15 +12,19 @@ chat_id = os.getenv("CHAT_ID")  # e.g., 123456789
 
 @TelegramRouter.post("/send-alert")
 async def send_telegram_alert(req: Request):
-    data = await req.json()
-    message = data.get("message", "No message received")
+    try:
+        data = await req.json()
+        message = data.get("message", "No message received")
 
-    telegram_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    payload = {
-        "chat_id": chat_id,
-        "text": message
-    }
+        telegram_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+        payload = {
+            "chat_id": chat_id,
+            "text": message
+        }
 
-    async with httpx.AsyncClient() as client:
-        response = await client.post(telegram_url, json=payload)
-        return {"telegram_response": response.json()}
+        async with httpx.AsyncClient() as client:
+            response = await client.post(telegram_url, json=payload)
+            return {"telegram_response": response.json()}
+    except Exception as e:
+        print(f"An Error occured on our site {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
